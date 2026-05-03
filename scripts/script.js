cdf31dedb2f60d5135c8b3a3471b65a9ec0f6ba5
IncertiHenri <henrique_incerti@estudante.sesisenai.org.br>
2026-05-03 10:20:15 -0300

    add - introdução

--- scripts/script.js
+++ scripts/script.js
@@ -6,18 +6,22 @@ document.getElementById("loginJogo").onsubmit = (e) => {
     let senha = Number(document.getElementById("senha").value);
     let verificacao = false;
 
-    if(!usuario) return alert("Informe um usuário válido!");
+    if (!usuario) return alert("Informe um usuário válido!");
 
-    if(!senha) return alert("Informe uma senha válida!");
+    if (!senha) return alert("Informe uma senha válida!");
 
     let usuarioCorreto = "demiurge";
     let senhaCorreta = 33550336;
 
-    if(usuario === usuarioCorreto && senha === senhaCorreta){
-        verificacao = true; 
+    if (usuario === usuarioCorreto && senha === senhaCorreta) {
+        verificacao = true;
         alert("Login realizado com sucesso!");
         window.location.href = "index2.html";
     } else {
         return alert("Nome de usuário ou senha incorretos!");
     }
+}
+
+function proximaPagina() {
+    window.location.href = "index3.html";
 }
